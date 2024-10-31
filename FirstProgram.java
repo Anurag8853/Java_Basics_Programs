3dc698ad825716ff835c81578e4a34ca39cdc887
Anurag8853 <whyanurag8853@gmail.com>
2024-10-31 14:17:06 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,64 @@
+class FirstProgram
+{
+	public static void main(String[] args)
+	{
+					
+		System.out.println("This is FirstProgram");
+		System.out.println("===Add numbers===");
+		System.out.println("12+30+42");
+		System.out.println(12+30+42);
+
+	}
+}
+class Program1
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println("This is program1");
+		System.out.println("===Subtract Numbers===");
+		System.out.println("120-30-42");
+		System.out.println(120-30-42);
+
+	
+
+
+
+	}
+}
+class Program2
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println("This is Program2");
+		System.out.println("===Multiply Numbers===");
+		System.out.println("120*30*42");
+		System.out.println(120*30*42);
+
+	}
+}
+class Program3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is Program3");
+		System.out.println("===Divide Numbers===");
+		System.out.println("120/30");
+		System.out.println(120/30);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
